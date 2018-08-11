2534f0ba84727c6bed45ae00f23ba5f75d76d50e
aswin4400 <aswin4400@gmail.com>
2018-08-11 22:42:27 +0530

    course data insertion

--- student/urls.py
+++ student/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
    path('view/<int:student_id>',views.student_view,name='student_view'),
    path('view/',views.view_students,name='view_students'),
    path('add/course/',views.course_add,name='course_add'),
-
+   path('course_update/<int:student_id>/<int:course_id>/',views.course_data,name='course_data'),
 ]
